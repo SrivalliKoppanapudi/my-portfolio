1c035290a94736b09a121e3b685e6444b88ad8ba
SrivalliKoppanapudi <srivallikoppanapudi@gmail.com>
2024-08-04 01:00:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const linksEl=document.querySelector(".links-toggle");
+const iconEl=document.querySelector("#icon i");
+const submitEl=document.getElementById("s-btn");
+const imgEl=document.querySelector(".prof1");
+iconEl.onclick=()=>{
+   // linksEl.classList.toggle("links-toggle")
+    linksEl.classList.toggle("active")
+    iconEl.classList.toggle("fa-x")
+    header.classList.toggle("sticky");
+
+}
+var typed=new Typed(".text",{
+    strings:["WEB DEVELOPER","FRONTEND EXPERTISE"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 50);
+
+});
